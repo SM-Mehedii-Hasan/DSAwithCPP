bf806e5df055e33440e9d99b5c1b5af3ef959a84
mehedi0hasan-git <sm_mehedi.hasan@outlook.com>
2025-08-14 15:52:03 +0600

    Create 2.hill then reverse hill or a symmetric triangle.cpp

---
+++ Pattern_Print/2.hill then reverse hill or a symmetric triangle.cpp
@@ -0,0 +1,33 @@
+//hill then reverse hill" or a symmetric triangle.
+#include<iostream>
+using namespace std;
+int main(){
+   int n;cin>>n;
+   int mid=n/2+1;
+      
+   for(int i=1;i<=n;i++){
+    if(i<=mid) {
+        for(int j=1;j<=i;j++) cout<<"* ";
+    }
+    
+     else{
+        for(int j=1;j<=n-i+1;j++){
+            cout<<"* ";
+        }
+     } 
+    cout<<endl;
+   }
+
+
+   }
+
+// * 
+// * * 
+// * * * 
+// * * * * 
+// * * * * * 
+// * * * * 
+// * * * 
+// * * 
+// * 
+
