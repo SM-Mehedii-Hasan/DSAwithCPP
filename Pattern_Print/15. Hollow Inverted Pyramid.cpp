04fb25c558e657507a179c8168e225de88889bc8
mehedi0hasan-git <sm_mehedi.hasan@outlook.com>
2025-08-14 23:34:24 +0600

    Create 15. Hollow Inverted Pyramid.cpp

---
+++ Pattern_Print/15. Hollow Inverted Pyramid.cpp
@@ -0,0 +1,26 @@
+//  Hollow Inverted Pyramid
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;cin >> n;
+    for(int i=1;i<=n;i++){
+
+        for(int j=1;j<=2*n-1;j++){
+            if(i==1||(i==n&&j==n)||j==i||j==2*n-i) cout<<"* ";
+            else  cout<<"  ";
+        }cout<<endl;
+
+
+    }
+  
+}
+
+// * * * * * * * * * 
+//   *           *   
+//     *       *     
+//       *   *       
+//         *         
+
+
