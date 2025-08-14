6e7299d3da7824afe64cc226b4e00861a1f7159b
mehedi0hasan-git <sm_mehedi.hasan@outlook.com>
2025-08-14 17:58:52 +0600

    Create 6. Inverted Right-Angled Triangle Star Pattern.cpp

---
+++ Pattern_Print/6. Inverted Right-Angled Triangle Star Pattern.cpp
@@ -0,0 +1,24 @@
+//  Inverted Right-Angled Triangle Star Pattern
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i+1;j++){
+            cout<<"*";
+        }cout<<endl;
+    }
+
+}
+
+// *********
+// ********
+// *******
+// ******
+// *****
+// ****
+// ***
+// **
+// *
