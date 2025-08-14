77bafed4eeb5a797e074d588d61ca484070f9b80
mehedi0hasan-git <sm_mehedi.hasan@outlook.com>
2025-08-14 17:46:44 +0600

    Create 5.diamond pattern .cpp

---
+++ Pattern_Print/5.diamond pattern .cpp
@@ -0,0 +1,39 @@
+// diamond pattern (flipped pyramid in the second half).
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int mid = n / 2 + 1;
+    for (int i = 1; i <= n; i++)
+    {
+        if (i <= mid)
+        {
+            for (int j = 1; j <= mid - i; j++)
+                cout << " ";
+            for (int j = 1; j <= 2 * i - 1; j++)
+                cout << "*";
+        }
+        else
+        {
+            for (int j = 1; j <= i - mid; j++)
+                cout << " ";
+            for (int j = 1; j <= 2 * (n - i) + 1; j++)
+                cout << "*";
+        }
+
+        cout << endl;
+    }
+}
+
+//     *
+//    ***
+//   *****
+//  *******
+// *********
+//  *******
+//   *****
+//    ***
+//     *
