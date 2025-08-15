5a3da32aeb0c58d439e6d840322cc54aff66942d
mehedi0hasan-git <sm_mehedi.hasan@outlook.com>
2025-08-16 01:17:02 +0600

    Create 23.Checkerboard pattern.cpp

---
+++ Pattern_Print/23.Checkerboard pattern.cpp
@@ -0,0 +1,38 @@
+//  Checkerboard pattern
+#include <iostream>
+using namespace std;
+int main()
+{
+   int n;
+   cin >> n;
+   for (int i = 1; i <= n; i++)
+   {
+      if (i % 2 != 0)
+      {
+         for (int j = 1; j <= 2 * n; j++)
+         {
+            if (j % 2 != 0)
+               cout << "* ";
+            else
+               cout << "  ";
+         }
+         cout << endl;
+      }
+      else
+      {
+         for (int j = 1; j <= 2 * n; j++)
+         {
+            if (j % 2 == 0)
+               cout << "* ";
+            else
+               cout << "  ";
+         }
+         cout << endl;
+      }
+   }
+}
+
+// *   *   *   *
+//   *   *   *   *
+// *   *   *   *
+//   *   *   *   *
