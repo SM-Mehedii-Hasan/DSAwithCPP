39b535f39a0d3ee409ce9b0cd809743c432d535d
mehedi0hasan-git <sm_mehedi.hasan@outlook.com>
2025-08-18 12:50:43 +0600

    Update k-th Pascal element.cpp

--- Pattern_Print/k-th Pascal element.cpp
+++ Pattern_Print/k-th Pascal element.cpp
@@ -1,52 +1,97 @@
-#include <iostream>
-using namespace std;
 
-int main()
-{
-  int r, c;
-  cin >> r >> c;
-  long long int res = 1;
+// __________1.Iterative multiplicative formula (without factorials)___________________________________________
 
 
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int r,c;cin>>r>>c;
+//  long long int val=1;
+//  for(int i=1;i<=c;i++){
+//     val=val*r;
+//     r--;
+//     val=val/i;
+//  }
+//   cout<<val<<endl;
 
-  // method 1:Efficient, avoids overflow Slightly more complex logic
-  for (int i = 1; i <= c; i++)
-  {
-    res *= r;
-    res /= i;
-    r--;
-  }
-  cout << res << endl;
-
-
-
-  // method 2:Simple logic Bigger intermediate numbers → overflow risk
-
-  // for(int i=1;i<=c;i++){
-  //     res *= r;
-  //     r--;
-  // }
-  // for(int j=1;j<=c;j++){
-  //     res /= j;
-  // }
-  // cout << res<<endl;
+//  }
 
 
 
 
-  // method 3:Simple factorial formula Slow for large r, high overflow risk
 
-  // long long int rfact=1, cfact=1, rcfact=1;
-  // for(int i=1;i<=r;i++) rfact *= i;
-  // for(int j=1;j<=c;j++) cfact *= j;
-  // for(int k=1;k<=r-c;k++) rcfact *= k;
-  // int element = rfact / (cfact * rcfact);
-  // cout << element << endl;
-}
+//________________________2.Iterative multiplicative formula (alternative indexing)_______________________________
 
-// Row 0:       1
-// Row 1:      1 1
-// Row 2:     1 2 1
-// Row 3:    1 3 3 1
-// Row 4:   1 4 6 4 1
 
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n, c;
+//     cin >> n >> c; 
+//     long long res = 1;
+
+//     for(int i=0; i<c; i++){
+//         res = res * (n - i);
+//         res = res / (i + 1);
+//     }
+
+//     cout << res << endl;
+// }
+
+
+
+
+//__________________________3.Iterative multiplicative formula with symmetry optimization___________________________________
+
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int r, c;
+//     cin >> r >> c;
+
+//     if (c > r - c) c = r - c; // use symmetry
+//     long long val = 1;
+
+//     for (int i = 1; i <= c; i++) {
+//         val = val * r;
+//         r--;
+//         val = val / i;
+//     }
+//     cout << val << endl;
+//     return 0;
+// }
+
+
+
+
+//________________________4.Factorial formula_____________________
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int r, c;
+//     cin >> r >> c;
+
+//     long long int rfact = 1, cfact = 1, rcfact = 1;
+
+//     for (int i = 1; i <= r; i++) 
+//         rfact *= i;
+
+    
+//     for (int j = 1; j <= c; j++) 
+//         cfact *= j;
+
+
+//     for (int k = 1; k <= r - c; k++) 
+//         rcfact *= k;
+
+ 
+//     long long int element = rfact / (cfact * rcfact);
+
+//     cout << element << endl;
+
+//     return 0;
+// }
