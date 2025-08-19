c85db28e1afde1bcbef35ae63dda06c136cc8348
sm_mehed1.H <sm_mehedi.hasan@outlook.com>
2025-08-19 18:44:41 +0600

    Create 191. Number of 1 Bits.cpp

---
+++ LeetCode_problems/191. Number of 1 Bits.cpp
@@ -0,0 +1,30 @@
+// 191. Number of 1 Bits
+// _____________________1: Bit manipulation (n & 1)__________________________________
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//    int n;cin>>n;
+//    int count=0;
+//    while(n!=0){
+//       if(n&1==1) count++;
+//      n= n>>1;
+
+//    }
+//    cout<<count<<endl;
+// }
+
+
+// ______Brian Kernighan’s Algorithm for counting set bits (Hamming weight).___________
+// #include<iostream>
+// using namespace std;
+// int main(){
+//    int n;cin>>n;
+//    int count =0;
+//    while(n){
+//        count++;
+//       n=n&n-1;
+//    }
+//    cout<<count<<endl;
+
+// }
