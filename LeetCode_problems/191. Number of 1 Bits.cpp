49d0a2e7bdabad3beca8e08d0beb2c6a9216ae27
sm_mehed1.H <sm_mehedi.hasan@outlook.com>
2025-08-19 19:29:58 +0600

    Update 191. Number of 1 Bits.cpp

--- LeetCode_problems/191. Number of 1 Bits.cpp
+++ LeetCode_problems/191. Number of 1 Bits.cpp
@@ -28,3 +28,10 @@
 //    cout<<count<<endl;
 
 // }
+//____________________Counting set bits (also called Hamming Weight or Population Count (popcount)).__________
+//#include<iostream>
+// using namespace std;
+// int main(){
+//    int n;cin>>n;
+//    cout<<__builtin_popcount(n)<<endl;
+// }
