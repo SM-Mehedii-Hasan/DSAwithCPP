55270637c5e1fa9ef8c362891ea230033531bc8a
sm_mehed1.H <sm_mehedi.hasan@outlook.com>
2025-08-19 21:12:03 +0600

    Create 190. Reverse Bits.cpp

---
+++ LeetCode_problems/190. Reverse Bits.cpp
@@ -0,0 +1,38 @@
+
+
+//___________1.Iterative Bit Manipulation______________
+// #include<iostream>
+// using namespace std;
+// int main(){
+//    int n;cin>>n;
+//    int reversed=0,bit;
+//    int x=32;
+//    while (x--)
+//    {
+//       bit=n&1;
+//       reversed=(reversed<<1)|bit;
+//       n=n>>1;
+//    }
+//    cout<<reversed<<endl;
+   
+// }
+
+// ___________________
+//  #include<iostream>
+
+// using namespace std;
+// int main(){
+//    int n;cin>>n;
+//    int reversed=0,bit;
+//    int x=32;
+//    for(int i=1;i<=x;i++)
+//    {
+//       bit=n&1;
+//       reversed=(reversed<<1)|bit;
+//       n=n>>1;
+//    }
+//    cout<<reversed<<endl;
+   
+// }
+
+
