7ff266dfc64f05f975a737731bd0a0250547f3f7
sm_mehed1.H <sm_mehedi.hasan@outlook.com>
2025-08-20 16:52:42 +0600

    Create 342. Power of Four.cpp

---
+++ LeetCode_problems/342. Power of Four.cpp
@@ -0,0 +1,40 @@
+
+//_________________    1.Iterative multiplication               __________________________
+// #include<iostream>
+// using namespace std;
+//     bool isPowerOfThree(int n) {
+//        int x=1;
+//        while(x<=n){
+//         if(x==n) return true;
+//         x*=4;
+//        }
+//        return false;
+        
+//     }
+//     int main(){
+//         int n;cin>>n;
+//         if(isPowerOfThree( n) ) cout<<n<<" is the power of four";
+//         else cout<<n<<" isn't the power of four";
+//     }
+
+// _________________________  2.Iterative division                     ________________________
+// #include<iostream>
+// using namespace std;
+//     bool isPowerOfThree(int n) {
+//       while(n%4==0){
+
+//         n/=4;
+//       }
+//       if(n==1) return true;
+//       else false;
+        
+//     }
+//     int main(){
+//         int n;cin>>n;
+//         if(isPowerOfThree( n) ) cout<<n<<" is the power of four";
+//         else cout<<n<<" isn't the power of four";
+//     }
+
+    // ______________________________3.
+// 
+    
