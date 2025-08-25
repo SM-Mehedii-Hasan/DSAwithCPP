b4342db18cdf0d36c993f4684bc261bb78029438
sm_mehed1.H <sm_mehedi.hasan@outlook.com>
2025-08-25 13:12:21 +0600

    Update 1.XOR of 1 to n Numbers.cpp

--- only_for_learning_purpose_and_curiosity/bitManipulation/1.XOR of 1 to n Numbers.cpp
+++ only_for_learning_purpose_and_curiosity/bitManipulation/1.XOR of 1 to n Numbers.cpp
@@ -1,1 +1,29 @@
 
+// #include<iostream>
+// using namespace std;
+// int soluttion(int n){
+//     int res=0;
+//     for(int i=1;i<=n;i++){
+//         res^=i;
+//     }
+//     return res;
+// }
+// int main(){
+//     int n;cin>>n;
+//     cout<<soluttion(n);
+    
+// }
+
+#include<iostream>
+using namespace std;
+int solution(int n){
+    if(n%4==0) return n;
+     else if(n%4==1) return 1;
+     else if(n%4==2) return n+1;
+    // else return 0;
+}
+int main(){
+    int n;cin>>n;
+    cout<<solution(n);
+
+}
