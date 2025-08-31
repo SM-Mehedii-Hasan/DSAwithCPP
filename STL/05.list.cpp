6158fa5d13a4e635501d0a9ca95b37e02eb1b3a4
sm_mehed1.H <sm_mehedi.hasan@outlook.com>
2025-08-31 22:19:39 +0600

    Update 05.list.cpp

--- STL/05.list.cpp
+++ STL/05.list.cpp
@@ -31,21 +31,59 @@
                     
 
 // }
-#include<bits/stdc++.h>
-using namespace std;
-int main(){
-    list<int>l={4,3,7,1,5,7,5,3,8,5,9,6,2};
-    cout<<"before sort:"<<endl;
-    for(auto x:l) cout<<x<<" ";
-    cout<<endl;
-    l.sort();
-     cout<<"after sort:"<<endl;
-    for(auto x:l) cout<<x<<" ";
-    cout<<endl;
-    l.reverse();
-      cout<<"after reverse:"<<endl;
-    for(auto x:l) cout<<x<<" ";
-    cout<<endl;
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     list<int>l={4,3,7,1,5,7,5,3,8,5,9,6,2};
+//     cout<<"before sort:"<<endl;
+//     for(auto x:l) cout<<x<<" ";
+//     cout<<endl;
+//     l.sort();
+//      cout<<"after sort:"<<endl;
+//     for(auto x:l) cout<<x<<" ";
+//     cout<<endl;
+//     l.reverse();
+//       cout<<"after reverse:"<<endl;
+//     for(auto x:l) cout<<x<<" ";
+//     cout<<endl;
   
 
-}
+// }
+//_____________merge list___________________
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     list<int>l1={1,2,3,4,5};
+//     list<int>l2={6,7,8,9};
+//      cout<<"before  merging:"<<endl;
+//     for(auto x:l1) cout<<x<<" ";
+//     cout<<endl;
+//     for(auto x:l2) cout<<x<<" ";
+//     cout<<endl;
+//     l1.merge(l2);
+//     cout<<"after merging:"<<endl;
+//     for(auto x:l1) cout<<x<<" ";
+//     cout<<endl;
+// }
+
+//__________swap list_____________________
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//       list<int>l1={1,2,3,4,5};
+//     list<int>l2={6,7,8,9};
+//       cout<<"before  swap:"<<endl;
+//     for(auto x:l1) cout<<x<<" ";
+//     cout<<endl;
+//     for(auto x:l2) cout<<x<<" ";
+//     cout<<endl;
+//     l1.swap(l2);
+//      cout<<"after  swap:"<<endl;
+//     for(auto x:l1) cout<<x<<" ";
+//     cout<<endl;
+//     for(auto x:l2) cout<<x<<" ";
+//     cout<<endl;
+
+    
+
+// }
